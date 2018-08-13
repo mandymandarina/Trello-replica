acc5a9862d6dcb1bad9ed01e792a4d194dd271a3
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-08-13 12:28:26 -0300

    cambios en parte de muro

--- src/js/modelo/modelo.js
+++ src/js/modelo/modelo.js
@@ -1,3 +1,4 @@
+
 window.filterFunction = () => {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
@@ -8,7 +9,7 @@ window.filterFunction = () => {
       if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
           a[i].style.display = "";
       } else {
-          a[i].style.display = "none";
+          a[i].style.display = "";
       }
   }
 }
--- src/js/vista/vista.js
+++ src/js/vista/vista.js
@@ -1,3 +1,16 @@
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
+// imprimir la tarjetaaddListaTar
+let btnAddListaTar = document.getElementById('addListaTar');
+const contAddTar = document.getElementById('sectionAddTar');
+const contPrint = document.getElementById('dorDivsAdd');
+
+btnAddListaTar.addEventListener('click', () =>{
+  const valAdd = document.getElementById('inputValAdd').value;
+  contPrint.style.display = 'block';
+  subContAddText.innerHTML += `<div class="subContText">${valAdd}<button class="btnDots">     <i class="fas fa-ellipsis-h"></i></button></div>`;
+  
+});
+
