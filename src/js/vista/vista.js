7790b009df94d49a18f26621277f8077b35d897d
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-08-14 12:09:33 -0300

    listo version responsive mobile first

--- src/js/vista/vista.js
+++ src/js/vista/vista.js
@@ -25,10 +25,19 @@ btnAddListaTar.addEventListener('click', () =>{
     console.log('Debe ingresar un texto');
     return false;    
   } else{
-    contPrint.style.display = 'block';
-    contPrint.style.float = 'left'  
-  subContAddText.innerHTML += `<div class="subContText">${valAdd}<button class="btnDots">     <i class="fas fa-ellipsis-h"></i></button></div>`;
+    contPrint.style.display = 'block'; 
+    sectionAddTar.style.display = 'none'; 
+  subContAddText.innerHTML += `<div class="subContText">${valAdd}<button class="btnDots"><i class="fas fa-ellipsis-h"></i></button></div>  
+  
+  <div id="otherAdd">
+  <div class="js-add-list list-wrapper mod-add" id="sectionAddTarMae"><form><span class="placeholder js-open-add-list"><span class="icon-sm icon-add">
+    <input class="list-name-input" id="inputValAdd" type="text" name="name" placeholder="Introduzca el título de la lista..." autocomplete="off" dir="auto" maxlength="512" value="">
+    <div class="list-add-controls u-clearfix">
+    <button class="primary mod-list-add-button js-save-edit" type="button" value="Añadir lista" id="addListaTar"><p>Añadir lista</p></button><i class="fas fa-times"></i><a class="icon-lg icon-close dark-hover js-cancel-edit" href="#"></a></div></form></div>
+  </div>`;
   document.getElementById('inputValAdd').value = '';
+
+
     return true
   }  
 });
